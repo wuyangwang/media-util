f70763c813df4034d820a722fb089cf245573f81
tardis <tardis@tardis.com>
2026-04-22 15:04:44 +0800

    update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,7 +12,6 @@ use tauri_plugin_store::StoreExt;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_single_instance::init(|app, args, cwd| {}))
         .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_fs::init())
